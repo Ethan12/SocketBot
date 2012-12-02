2f35c39c96446915f2fa2f8f95bbdcf91485f439
Ethan12 <ethanmcmullan1997@gmail.com>
2012-12-02 22:33:15 +0000

    Updates

--- src/com/ethan/SocketBot/Sockets.java
+++ src/com/ethan/SocketBot/Sockets.java
@@ -12,7 +12,7 @@ public class Sockets {
   public String line = null;
   public String joinMSG = "Hey";
   public BufferedWriter in;
-  public BufferedReade out;
+  public BufferedReader out;
     
   public void sendMessage(String channel, String message) throws IOException{
     in.write("PRIVMSG " + channel + " : " + message + "\r\n");
