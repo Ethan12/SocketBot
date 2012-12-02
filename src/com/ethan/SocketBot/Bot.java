1990a5ee5141a71153fac58c2fb5f9c6abb32ac2
Ethan12 <ethanmcmullan1997@gmail.com>
2012-12-02 22:51:40 +0000

    Update :D

--- src/com/ethan/SocketBot/Bot.java
+++ src/com/ethan/SocketBot/Bot.java
@@ -4,6 +4,7 @@ public class Bot {
     
     public static void main(String[] args) throws Exception{
         Sockets sock = new Sockets();
-         sock.connect("irc.cmpct.info", "Testie", "#bawt", 6667);
+        // e.g sock.connect("localhos", "Nick", "#channel", 6667);
+         sock.connect("", "", "", 6667);
     }
 }
