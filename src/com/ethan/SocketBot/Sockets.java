03a7f696b3f0b2b89f8e8e7683ac79c909b6a0b5
Ethan12 <ethanmcmullan1997@gmail.com>
2012-12-02 22:32:07 +0000

    l

--- src/com/ethan/SocketBot/Sockets.java
+++ src/com/ethan/SocketBot/Sockets.java
@@ -9,91 +9,88 @@ import java.util.List;
 
 public class Sockets {
     
-    public String line = null;
-    public String joinMSG = "Hey";
-    public BufferedWriter in;
-    public BufferedReader out;
+  public String line = null;
+  public String joinMSG = "Hey";
+  public BufferedWriter in;
+  public BufferedReader out;
     
-    public void sendMessage(String channel, String message) throws IOException{
-        in.write("PRIVMSG " + channel + " : " + message + "\r\n");
-        in.flush();
-    }
+  public void sendMessage(String channel, String message) throws IOException{
+    in.write("PRIVMSG " + channel + " : " + message + "\r\n");
+    in.flush();
+  }
+    
+  public void sendPacket(String packetType, String extra) throws IOException{
+    in.write(packetType + " " + extra + "\r\n");
+    in.flush();
+  }
 
-      public void connect(String server, String nick, String channel, Integer port) throws Exception {
-
+  public void connect(String server, String nick, String channel, Integer port) throws Exception {
+    Socket socket = new Socket(server, port);
+    in = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    out = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         
-        Socket socket = new Socket(server, port);
-        in = new BufferedWriter(
-                new OutputStreamWriter(socket.getOutputStream( )));
-        out = new BufferedReader(
-                new InputStreamReader(socket.getInputStream( )));
+    sendPacket("NICK", nick);
+    sendPacket("USER", nick +  " " +  nick + " " + nick + ": " + nick);
         
-        in.write("NICK " + nick + "\r\n");
-        in.write("USER " + nick + " " + nick + " " + nick + ": " + nick + "\r\n");
-        in.flush();
-        
-        
-        while ((line = out.readLine( )) != null) {
-            if (line.indexOf("004") >= 0) {
-                break;
-            }
-            else if (line.indexOf("433") >= 0) {
-                System.out.println("Nickname already taken.");
-                return;
-            }
+    while ((line = out.readLine( )) != null) {
+        if (line.indexOf("004") >= 0) {
+            break;
+        }else if (line.indexOf("433") >= 0) {
+         System.out.println("Nickname already taken.");
+          return;
         }
+      }
         
-        in.write("JOIN " + channel + "\r\n");
-        in.write("PRIVMSG " + channel + " : " + joinMSG +  "\r\n");
-        in.flush( );
+    in.write("JOIN " + channel + "\r\n");
+    sendMessage(channel, joinMSG);
+    in.flush();
         
-        while ((line = out.readLine()) != null) {
-            if (line.toLowerCase().startsWith("PING ")) {
-                in.write("PONG " + line.substring(5) + "\r\n");
-                in.write("PRIVMSG " + channel + " :ping!\r\n");
-                in.flush();
-            }else if (line.contains("PRIVMSG " + channel)){
-             String[] data = line.split(" ");
-             String cmdChar = data[3].substring(1);
-             String strArgument = "";
-             for(String e : data){
-                 if(e != null){
-                 strArgument = e;
-             }else{
-                strArgument = null;
-               }
-             }
-                switch (cmdChar.toUpperCase()) {
-                  case "!SAY":
-                      in.write("PRIVMSG " + channel + " : " + strArgument + "\r\n");
-                      in.flush();
-                      break;
-                  case "!SEARCH":
-                      String google = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
-                      String search = strArgument;
-                      String charset = "UTF-8";
+    while ((line = out.readLine()) != null) {
+      if (line.toLowerCase().startsWith("PING ")) {
+          in.write("PONG " + line.substring(5) + "\r\n");
+          in.write("PRIVMSG " + channel + " :ping!\r\n");
+          in.flush();
+          }else if (line.contains("PRIVMSG " + channel)){
+           String[] data = line.split(" ");
+           String cmdChar = data[3].substring(1);
+           String strArgument = "";
+          for(String e : data){
+              if(e != null){
+               strArgument = e;
+          }else{
+               strArgument = null;
+            }
+          }
+         switch (cmdChar.toUpperCase()) {
+             case "!SAY":
+                  in.write("PRIVMSG " + channel + " : " + strArgument + "\r\n");
+                  in.flush();
+                  break;
+             case "!SEARCH":
+                  String google = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
+                  String search = strArgument;
+                  String charset = "UTF-8";
 
-                      URL url = new URL(google + URLEncoder.encode(search, charset));
-                      Reader reader = new InputStreamReader(url.openStream(), charset);
-                      GoogleResults results = new Gson().fromJson(reader, GoogleResults.class);
+                  URL url = new URL(google + URLEncoder.encode(search, charset));
+                  Reader reader = new InputStreamReader(url.openStream(), charset);
+                  GoogleResults results = new Gson().fromJson(reader, GoogleResults.class);
 
-                      String title = results.getResponseData().getResults().get(0).getTitle();
-                      String urld = results.getResponseData().getResults().get(0).getUrl();
-                      sendMessage(channel, "Results of Google Search , Title: " + title + "URL:" + urld);
-                      break;
-                  default:
-                      System.out.println(line);
-                      break;
-                }
-            }else {
-                System.out.println(line);
+                  String title = results.getResponseData().getResults().get(0).getTitle();
+                  String urld = results.getResponseData().getResults().get(0).getUrl();
+                  sendMessage(channel, "Results of Google Search , Title: " + title + "URL:" + urld);
+                  break;
+              default:
+                  System.out.println(line);
+                  break;
+              }
+              }else {
+              System.out.println(line);
             }
         }
     }
-
 }
 
-  class GoogleResults {
+class GoogleResults {
 
     private ResponseData responseData;
     public ResponseData getResponseData() { return responseData; }
